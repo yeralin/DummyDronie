24bdb931075204d3dde821b25def53584f795a04
Daniyar Yeralin <yeralin@amazon.com>
2023-04-09 20:31:26 -0400

    Fix drone connection

--- DummyDronie/DJIConnector.swift
+++ DummyDronie/DJIConnector.swift
@@ -8,7 +8,9 @@
 import Foundation
 import DJISDK
 
-class DJIConnector: NSObject, DJISDKManagerDelegate, ObservableObject {
+class DJIConnector: NSObject, DJISDKManagerDelegate, DJIAppActivationManagerDelegate, ObservableObject {
+    
+    @Published private(set) var isConnected: Bool = false
     
     func registerWithSDK() {
         let appKey = Bundle.main.object(forInfoDictionaryKey: SDK_APP_KEY_INFO_PLIST_KEY) as? String
@@ -17,7 +19,7 @@ class DJIConnector: NSObject, DJISDKManagerDelegate, ObservableObject {
             log.error("Please enter your app key in the info.plist")
             return
         }
-        
+        log.info("Trying to register DJI SDK manager")
         DJISDKManager.registerApp(with: self)
     }
     
@@ -26,8 +28,23 @@ class DJIConnector: NSObject, DJISDKManagerDelegate, ObservableObject {
     }
     
     func appRegisteredWithError(_ error: Error?) {
-        log.error("SDK registered with an error: \(error?.localizedDescription ?? "Unknown")")
+        if let error = error {
+            log.error("SDK registered with an error: \(error.localizedDescription)")
+            return
+        }
+        log.info("Successfully registered the DJI SDK manager")
+        log.info("Trying to connect to DJI drone")
         DJISDKManager.startConnectionToProduct()
+    }
+    
+    func productConnected(_ product: DJIBaseProduct?) {
+        log.info("DJI drone connected")
+        isConnected = true
+    }
+    
+    func productDisconnected() {
+        log.info("DJI drone disconnected")
+        isConnected = false
     }
     
 }
