ee42a630c511ca20c248872cd586145409381258
Daniyar Yeralin <yeralin@amazon.com>
2023-04-11 12:20:45 -0400

    Properly handle DJI connection events

--- DummyDronie/DJIConnector.swift
+++ DummyDronie/DJIConnector.swift
@@ -10,7 +10,7 @@ import DJISDK
 
 class DJIConnector: NSObject, DJISDKManagerDelegate, DJIAppActivationManagerDelegate, ObservableObject {
     
-    @Published private(set) var isConnected: Bool = false
+    @Published private(set) var isDroneConnected: Bool = false
     
     func registerWithSDK() {
         let appKey = Bundle.main.object(forInfoDictionaryKey: SDK_APP_KEY_INFO_PLIST_KEY) as? String
@@ -20,7 +20,9 @@ class DJIConnector: NSObject, DJISDKManagerDelegate, DJIAppActivationManagerDele
             return
         }
         log.info("Trying to register DJI SDK manager")
-        DJISDKManager.registerApp(with: self)
+        DispatchQueue.global(qos: .userInitiated).async {
+            DJISDKManager.registerApp(with: self)
+        }
     }
     
     func didUpdateDatabaseDownloadProgress(_ progress: Progress) {
@@ -38,13 +40,31 @@ class DJIConnector: NSObject, DJISDKManagerDelegate, DJIAppActivationManagerDele
     }
     
     func productConnected(_ product: DJIBaseProduct?) {
-        log.info("DJI drone connected")
-        isConnected = true
+        guard let product = product else {
+            log.error("DJI product is not found")
+            return
+        }
+        
+        if product.model != "Only RemoteController" {
+            isDroneConnected = true
+        }
+        log.info("DJI product connected: \(product.model ?? "unknown")")
+    }
+    
+    func productChanged(_ product: DJIBaseProduct?) {
+        guard let product = product else {
+            log.error("DJI product is not found")
+            return
+        }
+        if product.model != "Only RemoteController" {
+            isDroneConnected = true
+        }
+        log.info("DJI product connected: \(product.model ?? "unknown")")
     }
     
     func productDisconnected() {
         log.info("DJI drone disconnected")
-        isConnected = false
+        isDroneConnected = false
     }
     
 }
